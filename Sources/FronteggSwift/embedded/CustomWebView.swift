cb888e75600897130a83e33e1343b534e59f5e24
Adam Rowe <adaminsley@gmail.com>
2026-09-14 16:35:49 -0400

    probe: temporary NSLog instrumentation (do not ship)

--- Sources/FronteggSwift/embedded/CustomWebView.swift
+++ Sources/FronteggSwift/embedded/CustomWebView.swift
@@ -1130,6 +1130,7 @@ class CustomWebView: WKWebView, WKNavigationDelegate, WKUIDelegate {
         logger.trace("didStartProvisionalNavigation")
         if let url = webView.url {
             let urlType = getOverrideUrlType(url: url)
+            NSLog("HLTHPROBE didStart urlType=\(urlType) path=\(url.path)")
             
             logger.info("urlType: \(urlType)")
             
@@ -1153,6 +1154,7 @@ class CustomWebView: WKWebView, WKNavigationDelegate, WKUIDelegate {
         logger.trace("didFinish")
         if let url = webView.url {
             let urlType = getOverrideUrlType(url: url)
+            NSLog("HLTHPROBE didFinish urlType=\(urlType) path=\(url.path)")
             logger.info("urlType: \(urlType), for: \(url.absoluteString)")
             
             // Update previousUrl for tracking unlock account flow
